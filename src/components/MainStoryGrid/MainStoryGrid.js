52f1b703a4399fe90343e391ea7f2ef8f5c294a2
Jørund B. Fagerjord <j.b.fagerjord@gmail.com>
2023-02-25 20:37:27 +0100

    fix: e#3, most of it

--- src/components/MainStoryGrid/MainStoryGrid.js
+++ src/components/MainStoryGrid/MainStoryGrid.js
@@ -12,6 +12,7 @@ import MainStory from '../MainStory';
 import SecondaryStory from '../SecondaryStory';
 import OpinionStory from '../OpinionStory';
 import Advertisement from '../Advertisement';
+import { QUERIES } from '../../constants';
 
 const MainStoryGrid = () => {
   return (
@@ -53,14 +54,46 @@ const Wrapper = styled.div`
     'advertisement';
   gap: 48px;
   margin-bottom: 48px;
+
+  @media ${QUERIES.tabletAndUp} {
+    grid-template-areas:
+      'main-story secondary-stories'
+      'advertisement advertisement'
+      'opinion-stories opinion-stories';
+    
+    grid-template-columns: 2fr 1fr;
+    gap: 16px;
+  }
+
+  @media ${QUERIES.laptopAndUp} {
+    grid-template-areas:
+      'main-story secondary-stories opinion-stories'
+      'main-story advertisement advertisement'
+    ;
+    grid-template-columns: 1fr calc(386px + 16px + 1px) 273px;
+  }
 `;
 
 const MainStorySection = styled.section`
   grid-area: main-story;
+
+  @media ${QUERIES.tabletAndUp} {
+    border-right: 1px solid var(--color-gray-300);
+    padding-right: 16px;
+  }
 `;
 
 const SecondaryStorySection = styled.section`
   grid-area: secondary-stories;
+
+  @media ${QUERIES.laptopAndUp} {
+    border-right: 1px solid var(--color-gray-300);
+    padding-right: 16px;
+  }
+`;
+
+const OpinionSection = styled.section`
+  grid-area: opinion-stories;
 `;
 
 const StoryList = styled.div`
@@ -74,14 +107,30 @@ const StoryList = styled.div`
     border-top: 1px solid var(--color-gray-300);
     padding-top: var(--spacing);
   }
+
+  ${OpinionSection} & {
+    @media ${QUERIES.tabletOnly} {
+      display: grid;
+      grid-template-columns: repeat(4, 1fr);
+    }
+  }
+
+  ${OpinionSection} & > * + * {
+    @media ${QUERIES.tabletOnly} {
+      border-top: revert;
+      padding-top: revert;
+    }
+  }
 `;
 
-const OpinionSection = styled.section`
-  grid-area: opinion-stories;
-`;
 
 const AdvertisementSection = styled.section`
   grid-area: advertisement;
+
+  @media ${QUERIES.laptopAndUp} {
+    border-top: 1px solid var(--color-gray-300);
+    padding-top: 16px;
+  }
 `;
 
 export default MainStoryGrid;
